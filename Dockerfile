721d3f1b3cd722f7e2732b797e0bc09c224e8648
Barend Erasmus <developersworkspace@gmail.com>
2017-06-07 11:52:03 +0200

    update

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@ FROM node:6.9.4
 
 RUN npm install pm2 -g
 
-CMD ["pm2-docker", "/opt/frontosa-pricelist-processor/app.js", "--", "--prod"]
+CMD ["pm2-docker", "/opt/frontosa-pricelist-processor/src/app.js", "--", "--prod"]
--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import xlsx from 'node-xlsx';
 import * as co from 'co';
 import * as mongo from 'mongodb';
 import * as path from 'path';
+import * as schedule from 'node-schedule';
 
 // Imports models
 import { Item } from './models/item';
@@ -14,70 +15,77 @@ let config = require('./config').config;
 const argv = require('yargs').argv;
 
 if (argv.prod) {
-  config = require('./config.prod').config;
+    config = require('./config.prod').config;
 }
 
+schedule.scheduleJob('42 * * * *', () => {
+    process();
+});
 
-co(function* () {
 
-    const filename: string = path.join(config.priceListDir, 'FrontosaPrice_2017-06-06.xls');
+function process() {
+    co(function* () {
 
-    const workSheetsFromFile = xlsx.parse(filename);
+        const filename: string = path.join(config.priceListDir, 'FrontosaPrice_2017-06-06.xls');
 
-    const processedRows: ProcessedRow[] = [];
-    let currentHeaderColumns: string[] = null;
+        const workSheetsFromFile = xlsx.parse(filename);
 
-    for (const sheet of workSheetsFromFile.filter((x) => ['Main', 'consumable', 'NoteBook', 'mobile', 'Full PC', 'consumer'].indexOf(x.name) > -1)) {
-        for (const row of sheet.data) {
-            if (!isRowValid(row)) {
+        const processedRows: ProcessedRow[] = [];
+        let currentHeaderColumns: string[] = null;
+
+        for (const sheet of workSheetsFromFile.filter((x) => ['Main', 'consumable', 'NoteBook', 'mobile', 'Full PC', 'consumer'].indexOf(x.name) > -1)) {
+            for (const row of sheet.data) {
+                if (!isRowValid(row)) {
+                    continue;
+                }
+
+                if (isRowHeader(row)) {
+                    currentHeaderColumns = row;
+                }
+
+                if (isRowItem(row)) {
+                    processedRows.push(new ProcessedRow(currentHeaderColumns, row, sheet.name));
+                }
+            }
+        }
+
+        const items = processedRows.map((x) => Item.fromProcessedRow(x));
+        const filteredItems: Item[] = [];
+        for (const item of items) {
+            if (filteredItems.find((x) => x.code == item.code) === undefined) {
+                filteredItems.push(item);
+            }
+        }
+
+        const db: mongo.Db = yield mongo.MongoClient.connect('mongodb://localhost:27017/frontosa');
+
+        const collection: mongo.Collection = db.collection('items');
+
+        // yield collection.remove({});
+
+        for (const item of filteredItems) {
+            const databaseItem = yield collection.findOne({
+                hash: item.hash
+            });
+
+            if (databaseItem) {
                 continue;
             }
 
-            if (isRowHeader(row)) {
-                currentHeaderColumns = row;
-            }
-
-            if (isRowItem(row)) {
-                processedRows.push(new ProcessedRow(currentHeaderColumns, row, sheet.name));
-            }
-        }
-    }
-
-    const items = processedRows.map((x) => Item.fromProcessedRow(x));
-    const filteredItems: Item[] = [];
-    for (const item of items) {
-        if (filteredItems.find((x) => x.code == item.code) === undefined) {
-            filteredItems.push(item);
-        }
-    }
-
-    const db: mongo.Db = yield mongo.MongoClient.connect('mongodb://localhost:27017/frontosa');
-
-    const collection: mongo.Collection = db.collection('items');
-
-    // yield collection.remove({});
-
-    for (const item of filteredItems) {
-        const databaseItem = yield collection.findOne({
-            hash: item.hash
-        });
-        
-        if (databaseItem) {
-            continue;
+            console.log(`Inserting ${item.code}`);
+            yield collection.insertOne({
+                code: item.code,
+                hash: item.hash,
+                attributes: item.attributes,
+                description: item.attributes,
+                price: item.price
+            });
         }
 
-        console.log(`Inserting ${item.code}`);
-        yield collection.insertOne({
-            code: item.code,
-            hash: item.hash,
-            attributes: item.attributes,
-            description: item.attributes,
-            price: item.price
-        });
-    }
+        db.close();
+    });
 
-    db.close();
-});
+}
 
 function isRowValid(row): boolean {
 
